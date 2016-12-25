c8f7164dbef657b91e55ef8631f05b63216128ab
nikitaevg <vorobyev.nikita@gmail.com>
2016-12-25 12:14:56 +0300

    Minor fixes

--- homework-3/bind.h
+++ homework-3/bind.h
@@ -34,19 +34,19 @@ private:
     }
 
     template <typename old_Arg, typename... new_Args>
-    auto&& arg_get(const old_Arg& old_arg, new_Args&...) const
+    auto&& arg_get(old_Arg& old_arg, new_Args&...)
     {
         return old_arg;
     };
     
     template <typename Fn, typename... old_Args, typename... new_Args>
-    auto arg_get(const bind_t<Fn, old_Args...>& b, new_Args&... new_args) const
+    auto arg_get(bind_t<Fn, old_Args...>& b, new_Args&... new_args)
     {
         return b(new_args...);
     }
     
     template <int N, typename... new_Args>
-    auto&& arg_get(const place_holder<N>&, new_Args&... new_args) const
+    auto&& arg_get(place_holder<N>&, new_Args&... new_args)
     {
         return std::get<N>(std::forward_as_tuple(new_args...));
     }
@@ -68,23 +68,23 @@ private:
     };
     
     template <typename... new_Args, int... S>
-    auto call_impl1(const sequence<S...>&, new_Args&&... new_args) const
+    auto call_impl1(const sequence<S...>&, new_Args&&... new_args)
     {
         return func(arg_get(std::get<S>(args), new_args...)...);
     }
     
     template <typename... new_Args>
-    auto call(new_Args&&... new_args) const
+    auto call(new_Args&&... new_args)
     {
         return call_impl1(typename unpacker<std::tuple_size<tuple_>::value>::type(), std::forward<new_Args>(new_args)...);
     }
     
     template <typename F_, typename... Args_>
-    friend bind_t<F_, Args_...> bind(F_&& func, Args_&&... args);
+    friend bind_t<F_, Args_...> bind(F_&& func, Args_... args);
     
 public:
     template <typename... new_Args>
-    auto operator()(new_Args&&... new_args) const
+    auto operator()(new_Args&&... new_args) 
     {
         return call(std::forward<new_Args>(new_args)...);
     }
@@ -92,7 +92,7 @@ public:
 };
 
 template <typename F, typename... Args>
-bind_t<F, Args...> bind(F&& func, Args&&... args)
+bind_t<F, Args...> bind(F&& func, Args... args)
 {
     return bind_t<F, Args...>(std::forward<F>(func), std::forward<Args>(args)...);
 }
