2385549e930c773b9954dc42c5cf487c7ff036c3
nikitaevg <vorobyev.nikita@gmail.com>
2016-12-28 18:56:32 +0300

    Fix

--- homework-3/bind.h
+++ homework-3/bind.h
@@ -14,7 +14,6 @@ place_holder<2> _3;
 place_holder<3> _4;
 place_holder<4> _5;
 
-
 template <typename Fp, typename... Args>
 struct bind_t
 {
@@ -22,32 +21,32 @@ struct bind_t
 private:
     
     typedef typename std::decay<Fp>::type F;
-    typedef std::tuple<Args...> tuple_;
+    typedef std::tuple<std::decay_t<Args>...> tuple_;
     
     F func;
     tuple_ args;
     
-    template <typename F_>
-    bind_t(F_&& func, Args&&... args) :
+    template <typename F_, typename... Args_>
+    bind_t(F_&& func, Args_&&... args) :
     func(std::forward<F_>(func)),
-    args(std::move(args)...)
+    args(std::forward<Args_>(args)...)
     {
     }
 
     template <typename old_Arg, typename... new_Args>
-    auto&& arg_get(old_Arg& old_arg, new_Args&...)
+    decltype(auto) arg_get(old_Arg& old_arg, new_Args&...)
     {
         return old_arg;
     };
     
     template <typename Fn, typename... old_Args, typename... new_Args>
-    auto arg_get(bind_t<Fn, old_Args...>& b, new_Args&... new_args)
+    decltype(auto) arg_get(bind_t<Fn, old_Args...>& b, new_Args&... new_args)
     {
         return b(new_args...);
     }
     
     template <int N, typename... new_Args>
-    auto&& arg_get(place_holder<N>&, new_Args&... new_args)
+    decltype(auto) arg_get(place_holder<N>&, new_Args&... new_args)
     {
         return std::get<N>(std::forward_as_tuple(new_args...));
     }
@@ -81,7 +80,7 @@ private:
     }
     
     template <typename F_, typename... Args_>
-    friend bind_t<F_, Args_...> bind(F_&& func, Args_... args);
+    friend bind_t<F_, Args_...> bind(F_&& func, Args_&&... args);
     
 public:
     template <typename... new_Args>
@@ -93,9 +92,9 @@ public:
 };
 
 template <typename F, typename... Args>
-bind_t<F, Args...> bind(F&& func, Args... args)
+bind_t<F, Args...> bind(F&& func, Args&&... args)
 {
-    return bind_t<F, Args...>(std::forward<F>(func), std::move(args)...);
+    return bind_t<F, Args...>(std::forward<F>(func), std::forward<Args>(args)...);
 }
 
 
